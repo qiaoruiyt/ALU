4bb441e77ddd2b5b5e1297ba504cdb1f7f2a052d
polarvenezia <polarvenezia@gmail.com>
2016-10-20 23:05:17 +0800

    ALU

---
+++ ALU.mojo
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project name="ALU" board="Mojo V3" language="Lucid">
+  <files>
+    <src>alu.luc</src>
+    <src>test_case.luc</src>
+    <src>segments.luc</src>
+    <src>Boolean.luc</src>
+    <src>adder.luc</src>
+    <src>multiple_divide.luc</src>
+    <src top="true">mojo_top.luc</src>
+    <src>comparator.luc</src>
+    <src>alu_module.luc</src>
+    <src>blinker.luc</src>
+    <src>Shift.luc</src>
+    <ucf lib="true">io_shield.ucf</ucf>
+    <ucf lib="true">mojo.ucf</ucf>
+    <component>decoder.luc</component>
+    <component>reset_conditioner.luc</component>
+    <component>counter.luc</component>
+  </files>
+</project>
---
+++ work/project.tcl
@@ -0,0 +1,22 @@
+set projDir "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/planAhead"
+set projName "ALU"
+set topName top
+set device xc6slx9-2tqg144
+if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
+create_project $projName "$projDir/$projName" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/mojo_top_0.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/segments_1.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/reset_conditioner_2.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/test_case_3.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/counter_4.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/blinker_5.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/alu_6.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/Boolean_7.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/adder_8.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/comparator_9.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/Shift_10.v" "D:/aWork/SUTDTerm4/Computational Structure/MojoWorkspace/ALU/work/verilog/multiple_divide_11.v"]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set ucfSources [list  "D:/Install/Mojo\ IDE/library/components/io_shield.ucf" "D:/Install/Mojo\ IDE/library/components/mojo.ucf"]
+import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
+set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
+set_property steps.map.args.mt on [get_runs impl_1]
+set_property steps.map.args.pr b [get_runs impl_1]
+set_property steps.par.args.mt on [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1
+wait_on_run synth_1
+launch_runs -runs impl_1
+wait_on_run impl_1
+launch_runs impl_1 -to_step Bitgen
+wait_on_run impl_1
